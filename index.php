4a9e72392a54ab0050a283d30037a314c2b2f47d
LaurenceB28 <laurence.brd@gmail.com>
2020-11-19 12:10:05 +0100

    exo1phpP3

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exophp1P3</title>
+</head>
+<body>
+<?php
+// La fonction array permet de créer un array
+// $months = array ('Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre');
+$months[0] = 'Janvier';
+$months[1] = 'Fevrier';
+$months[2] = 'Mars';
+$months[3] = 'Avril';
+$months[4] = 'Mai';
+$months[5] = 'Juin';
+$months[6] = 'Juillet';
+$months[7] = 'Aout';
+$months[8] = 'Septembre';
+$months[9] = 'Octobre';
+$months[10] = 'Novembre';
+$months[11] = 'Decembre';
+
+?>
+</body>
+</html>
